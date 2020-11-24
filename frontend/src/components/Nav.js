791b6834f5557b531f3988e7360351e4c77a6f58
Mhaelgaidin <d_hooper@hotmail.co.uk>
2020-11-24 15:22:48 +0000

    added mobile responsiveness

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Nav = ({ recipes, selectRecipe, activeRecipe }) => {
+  const [showNav, setShowNav] = useState(true)
+
   const checkActive = (recipe) => {
     if (activeRecipe.name === recipe.name) {
       return 'active'
@@ -9,18 +11,53 @@ const Nav = ({ recipes, selectRecipe, activeRecipe }) => {
     }
   }
 
+  const handleClick = (index) => {
+    selectRecipe(index)
+    toggleMenu()
+  }
+
+  const handleResize = () => {
+    if (window.innerWidth < 600) {
+      setShowNav(false)
+    } else {
+      setShowNav(true)
+    }
+  }
+
+  const toggleMenu = () => {
+    if (window.innerWidth < 600) {
+      setShowNav(showNav ? false : true)
+    }
+  }
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+  }, [])
+
   return (
     <nav>
-      <h1>MyRecipes</h1>
-      {recipes.map((recipe, index) => (
-        <p
-          key={index}
-          onClick={selectRecipe.bind(this, index)}
-          className={checkActive(recipe)}
-        >
-          {recipe.name}
-        </p>
-      ))}
+      <h1 onClick={toggleMenu}>
+        MyRecipes
+        {showNav ? (
+          <i class='fas fa-folder-open'></i>
+        ) : (
+          <i class='fas fa-folder'></i>
+        )}
+      </h1>
+      {showNav && (
+        <div>
+          {recipes.map((recipe, index) => (
+            <p
+              key={index}
+              onClick={handleClick.bind(this, index)}
+              className={checkActive(recipe)}
+            >
+              {recipe.name}
+            </p>
+          ))}
+        </div>
+      )}
     </nav>
   )
 }
